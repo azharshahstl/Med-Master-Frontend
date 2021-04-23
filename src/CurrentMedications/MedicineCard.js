6f193960026733259494e0bd3b88dfb16d15523d
MullaN <admullan@gmail.com>
2021-04-23 15:53:05 -0700

    added more stlying to medicine cards

--- src/CurrentMedications/MedicineCard.js
+++ src/CurrentMedications/MedicineCard.js
@@ -3,9 +3,18 @@ import React from 'react';
 const MedicineCard = ({medicine}) => {
     return(
         <div style={styles.medicineCard}>
-            <p>{medicine.name}</p>
-            <p>{medicine.dosage}</p>
-            <p>{medicine.startDate}</p>
+            <div style={styles.cardRow}>
+                <span>{medicine.name}</span>
+                <span>{medicine.dosage}</span>
+            </div>
+            <div style={styles.cardRow}>
+                <span>Start Date</span>
+                <span>{medicine.startDate}</span>
+            </div>
+            <div style={styles.cardRow}>
+                <button>Delete Medication</button>
+                <button>Change Dosage</button>
+            </div>
         </div>
     )
 }
@@ -14,17 +23,22 @@ const styles = {
     medicineCard:{
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        width: '10vh',
-        height: '10vh',
-        padding: '5vh',
+        width: '30vh',
+        height: '30vh',
+        padding: '3vh',
         margin: '5vh',
         borderStyle: 'solid',
         borderWidth: '1px',
         borderColor: 'black',
         borderRadius: '15px',
         boxShadow: '10px 10px 8px #888888'
+    },
+    cardRow:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: 'inherit'
     }
 }
 
