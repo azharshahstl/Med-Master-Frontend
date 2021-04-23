abb62f436daaf1c25782905c48cabe4fbb04a1b7
azharshahstl <azharshahstl@yahoo.com>
2021-04-23 15:30:38 -0500

    Working on modals

--- src/index.js
+++ src/index.js
@@ -3,28 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { combineReducers } from 'redux';
+// import {createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { combineReducers } from 'redux';
 
-// Need to create the 2 reducers
-const rootReducer = combineReducers({
-  user: usersReducer,
-  medicines: medicinesReducer
-})
+// // Need to create the 2 reducers
+// const rootReducer = combineReducers({
+//   user: usersReducer,
+//   medicines: medicinesReducer
+// })
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+      <App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
