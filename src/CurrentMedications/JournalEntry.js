592954d539173bd0b145e4804011c22d3df8818d
MullaN <admullan@gmail.com>
2021-04-24 09:59:38 -0700

    made journal entry form controlled

--- src/CurrentMedications/JournalEntry.js
+++ src/CurrentMedications/JournalEntry.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const JournalEntry = ({date, closeEntry}) => {
+    const [entryData, setEntryData] = useState('')
+
     return (
         <div style={styles.journalEntryContainer}>
             <div style={styles.journalEntryCard}>
                 <h3>Journal Entry for {date}</h3>
-                <textarea placeholder='How are you feeling today...' style={styles.entryTextArea}/>
-                <button>Save</button>
-                <button onClick={() => closeEntry()}>Discard</button>
+                <textarea placeholder='How are you feeling today...' value={entryData} onChange={(e) => setEntryData(e.target.value)} style={styles.entryTextArea}/>
+                <div>
+                    <button style={styles.entryBtns}>Save</button>
+                    <button onClick={() => closeEntry()} style={styles.entryBtns}>Discard</button>
+                </div>
             </div>
         </div>
     )
@@ -34,10 +38,19 @@ const styles = {
         padding: '3vh',
         boxShadow: '10px 10px 8px #888888',
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        
     },
     entryTextArea: {
         height: '75%'
+    },
+    entryBtns: {
+        width: '50vh',
+        height: '5vh',
+        backgroundColor: 'darkslateblue',
+        borderRadius: '15px',
+        color: 'white',
+        fontSize: '3vh'
     }
 }
 
